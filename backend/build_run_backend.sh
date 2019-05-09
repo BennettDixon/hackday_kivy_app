74baf84b764a0b6a0c0c36419d3fe7221ebdab13
BennettDixon <bennettdixon16@gmail.com>
2019-05-09 19:22:21 +0000

    switched startup script for docker-compose which is preferable for maintainability

--- backend/build_run_backend.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# script for building and spawning a backend
-HOST_PORT=5000
-CONT_PORT=5000
-
-docker build -t bennettdixon16/hackday_kivy_app -f Dockerfile.dev .
-echo -e "\n\nforwarding port $CONT_PORT from the docker container to port $HOST_PORT on the host\n\n"
-docker run -p "$HOST_PORT:$CONT_PORT" bennettdixon16/hackday_kivy_app
