bae78e71e36352dca81fcf441e0a61086bb39d67
BennettDixon <bennettdixon16@gmail.com>
2019-05-09 19:09:12 +0000

    updated backend build test script to output forwarded port info

--- backend/build_run_backend.sh
+++ backend/build_run_backend.sh
@@ -4,4 +4,5 @@ HOST_PORT=5000
 CONT_PORT=5000
 
 docker build -t bennettdixon16/hackday_kivy_app -f Dockerfile.dev .
-docker run -p "$HOST_PORT":"$CONT_PORT" bennettdixon16/hackday_kivy_app
+echo -e "\n\nforwarding port $CONT_PORT from the docker container to port $HOST_PORT on the host\n\n"
+docker run -p "$HOST_PORT:$CONT_PORT" bennettdixon16/hackday_kivy_app
