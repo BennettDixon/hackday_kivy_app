eca2702f057c5c9b4633116af01535a14f7c9d2b
Wescott Sharples <wescottsharples@Wescotts-MacBook-Pro.local>
2019-05-10 14:18:10 -0700

    Add drag and drop file support

--- frontend/kivy_factory.py
+++ frontend/kivy_factory.py
@@ -12,13 +12,20 @@ from kivy.properties import StringProperty
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 from .tools import verify_image, convert_image
+from kivy.core.window import Window
 
 
 class GetFilename(BoxLayout, Screen):
     """Defines the GetFileName class -> Screen for manually inputting image pathname"""
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        Window.bind(on_dropfile=self._on_file_drop)
+    
+
     load = ObjectProperty(None)
     cancel = ObjectProperty(None)
     filename_write = StringProperty('')
+    filepath = StringProperty('')
     
     def store_filename_write(self, *args):
         filename = self.ids['filename_text'].text
@@ -33,6 +40,7 @@ class GetFilename(BoxLayout, Screen):
     def load(self, path, filename, *args):
         if len(filename) < 1:
             return False
+        self.manager.get_screen('main').filepath = filename[0]
         if verify_image(filename[0]) is False:
             return False
         json = convert_image(filename[0])
@@ -40,6 +48,11 @@ class GetFilename(BoxLayout, Screen):
         self.manager.get_screen('results').api_results = results
         self.manager.get_screen('results').source = filename[0]
         return True
+
+    def _on_file_drop(self, window, filepath):
+        image = filepath.decode('utf-8')
+        self.manager.get_screen('main').filepath = image
+        
 
 class Results(Screen):
     api_results = StringProperty('')
