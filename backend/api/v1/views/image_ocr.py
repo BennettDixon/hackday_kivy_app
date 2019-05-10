e84f3e9317c08c7322c0fb2cded8185aafb3738c
jozsa <allisonjoyweiner@gmail.com>
2019-05-10 05:04:28 +0000

    Call right part of dictionary from Azure API & update API request link

--- backend/api/v1/views/image_ocr.py
+++ backend/api/v1/views/image_ocr.py
@@ -25,20 +25,20 @@ def process_image(img):
     assert subscription_key
 
     # Azure Vision API URL for detecting objects
-    detection_url = 'https://westus.api.cognitive.microsoft.com/vision/v2.0/detect'
+    detection_url = 'https://westus.api.cognitive.microsoft.com/vision/v2.0/analyze'
 
     headers = {'Ocp-Apim-Subscription-Key': subscription_key,
-               'Content-Type': 'application/json'}
+               'Content-Type': 'application/octet-stream'}
 
     # POST request to Azure API to get image data
     response = requests.post(analyze_url, headers=headers, data=img_data)
 
-    image_data = response.json()
+    image_data = response.json().get('captions')
     if image_data is None or type(image_data) is not dict:
         return jsonify({'error': 'Not a JSON'}), 400
 
     try:
-        caption_response = CaptionResponse(image_data.get('caption'),
+        caption_response = CaptionResponse(image_data.get('text'),
                                            image_data.get('confidence'))
     except (ValueError, TypeError) as error:
         abort(500) # TODO - verify this abort method, should we return a JSON error dict with a code?
