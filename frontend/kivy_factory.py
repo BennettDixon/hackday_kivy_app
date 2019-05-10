fffa48af9e9221254e1447d7d091b12b510adcb8
Susan Su <susansu.software@gmail.com>
2019-05-10 02:38:15 -0700

    Beginnings of the kivy frontend

---
+++ frontend/kivy_factory.py
@@ -0,0 +1,44 @@
+"""Defines the ImageOCRApp class"""
+from kivy.app import App
+
+# Import widgets from kivy
+from kivy.uix.button import Button
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.textinput import TextInput
+from kivy.uix.boxlayout import BoxLayout
+from kivy.factory import Factory
+from kivy.properties import ObjectProperty
+from kivy.properties import StringProperty
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.lang import Builder
+
+
+class GetFilename(BoxLayout, Screen):
+    load = ObjectProperty(None)
+    cancel = ObjectProperty(None)
+    filename_write = StringProperty('')
+    
+    def store_filename_write(self, *args):
+        filename = self.ids['filename_text'].text
+        return True
+        
+class Results(Screen):
+
+    @classmethod
+    def get_image(self, filename):
+        print('in get_image')
+        self.result = 'I CHANGED'
+
+class WindowManager(ScreenManager):
+    pass
+
+kv = Builder.load_file('imageocr.kv')
+
+class ImageOCRApp(App):
+    """Defines the ImageOCR Application Class"""
+    def build(self):
+        """This method is called when run() method is called"""
+        return kv
+
+if __name__ == '__main__':
+    ImageOCRApp().run()
