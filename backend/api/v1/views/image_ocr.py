2b8f86bd65adf5077bf1218bc51d552caea016fe
jozsa <allisonjoyweiner@gmail.com>
2019-05-10 02:37:29 +0000

    Variable names changed, comments added, basic functionality

--- backend/api/v1/views/image_ocr.py
+++ backend/api/v1/views/image_ocr.py
@@ -1,26 +1,50 @@
 #!/usr/bin/python3
 """
-handles REST API actions for place
+Handles processing of images and sending
+them to the Vision Azure API.
 """
 from api.v1.views import app_views
-from flask import jsonify
-from flask import Flask
-from flask import request
-from flask import abort
-from os import getenv
+from flask import Flask, jsonify, request, abort
+import requests
 
 @app_views.route(
     '/image_ocr',
     methods=['POST'],
     strict_slashes=False)
-def places_search():
-    """processes an image and returns the text contained in it
+
+def process_image(img):
+    """Processes an image by:
+        -> saving it into a file #TODO remove this line if we decide not to go with this method
+        -> sending the file to Vision Azure api
+        -> returns a dictionary containing the caption
+        and confidence level associated with that image
         TODO implement
     """
-    post_data = request.get_json()
-    places_search = []
-    if post_data is None or type(post_data) != dict:
+    # Verify Azure Vision Subscription Key
+    subscription_key = vision_subscription_key
+    assert subscription_key
+
+    # Azure Vision API URL for detecting objects
+    detection_url = 'https://westus.api.cognitive.microsoft.com/vision/v2.0/detect'
+
+    headers = {'Ocp-Apim-Subscription-Key': subscription_key,
+               'Content-Type': 'application/json'}
+
+    # POST request to Azure API to get image data
+    response = requests.post(analyze_url, headers=headers, data=img_data)
+
+    image_data = response.json()
+    if image_data is None or type(image_data) is not dict:
         return jsonify({'error': 'Not a JSON'}), 400
+
+    try:
+        caption_response = CaptionResponse(post_data.get('caption'),
+                                           post_data.get('confidence'))
+    except ValueError, TypeError:
+        abort(500) # TODO - verify this abort method, should we return a JSON error dict with a code?
+    return caption_response.to_dict()
+
+
 
 @app_views.route('/testing', methods=['GET'], strict_slashes=False)
 def testing_api():
